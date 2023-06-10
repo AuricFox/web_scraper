414e666f80303a1838323693ac27408411e99ca0
kitze012 <sskitzerow@gmail.com>
2023-06-10 16:12:57 -0500

    Getting data from target page

--- taxscraper/taxscraper/spiders/taxspider.py
+++ taxscraper/taxscraper/spiders/taxspider.py
@@ -11,10 +11,10 @@ class TaxSpider(scrapy.Spider):
     # Used for iterating thru pages and passing the data to the parse_page function
     def parse(self, response):
         table_rows = response.xpath('//tbody/tr')                   # Get data in the table body
-        logging.info(f'Size of data: {len(table_rows)}')
+        logging.info(f'Size of data: {len(table_rows)} | Response: {response}')
 
         if len(table_rows) == 0:                                    # Last page reached, parse it
-            yield self.parse_page(response)
+            yield from self.parse_page(response)
 
         # Iterate thru all the rows in the table and get the links to the next page
         for data in table_rows:
@@ -29,7 +29,7 @@ class TaxSpider(scrapy.Spider):
             '''
             if next_page is None: continue                          # Ignore rows without links
 
-            if(self.start_urls[0] not in next_page):                # Invalid relative url
+            if self.start_urls[0] not in next_page:                # Invalid relative url
                 subpage = next_page.split('statutes/')[-1]          # Get last part of relative url
                 next_page = f'{self.start_urls[0]}{subpage}'        # Append absolute and relative url
             
@@ -37,16 +37,20 @@ class TaxSpider(scrapy.Spider):
             yield response.follow(next_page, callback=self.parse)   # Recursively pass next page 
     
     # ===============================================================================
-    # Used as a helper function for parsing the html from target pages
+    # Used as a helper function for parsing the text from target pages
     def parse_page(self, res):
-        logging.info(f'Response Status Code: {res.status}')
-        #data = res.css('''TARGET TAG''')
+        print("===============================================================================")
+        
+        # Extract all the text withing the page section
+        data = res.xpath('//div[@class="section"]//text()').extract()
+        logging.info(f'\nResponse: {res}\nData: {data}')
 
         # Collect target data here
-        yield {
-            'url': res.url,
+        yield None
+        '''yield {
+            'url': res,
             'title': res.css('.title').get()
-        }
+        }'''
 
 # Adjust the logging settings
 logging.getLogger('scrapy').propagate = False
