c446ef1f2f2765b5d7a562bb5042b876ea628db0
kitze012 <sskitzerow@gmail.com>
2023-06-11 00:05:43 -0500

    Scraping data and setting up items

--- taxscraper/taxscraper/items.py
+++ taxscraper/taxscraper/items.py
@@ -1,12 +1,10 @@
 # Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
-class TaxscraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class TaxItem(scrapy.Item):
+
+    statute = scrapy.Field()
+    section_number = scrapy.Field()
+    subdiv_number = scrapy.Field()
+    info = scrapy.Field()
--- taxscraper/taxscraper/spiders/taxspider.py
+++ taxscraper/taxscraper/spiders/taxspider.py
@@ -1,5 +1,6 @@
 import scrapy
 import logging
+from taxscraper.items import TaxItem
 
 class TaxSpider(scrapy.Spider):
     name = "taxspider"
@@ -11,7 +12,7 @@ class TaxSpider(scrapy.Spider):
     # Used for iterating thru pages and passing the data to the parse_page function
     def parse(self, response):
         table_rows = response.xpath('//tbody/tr')                   # Get data in the table body
-        logging.info(f'Size of data: {len(table_rows)} | Response: {response}')
+        #logging.info(f'Size of data: {len(table_rows)} | Response: {response}')
 
         if len(table_rows) == 0:                                    # Last page reached, parse it
             yield from self.parse_page(response)
@@ -19,7 +20,7 @@ class TaxSpider(scrapy.Spider):
         # Iterate thru all the rows in the table and get the links to the next page
         for data in table_rows:
             next_page = data.xpath('.//a/@href').get()              # Get the next page link
-            #logging.info(f'Next Page -> {next_page} | Size of data: {len(table_rows)}')
+            
             '''
             Some pages have absolute urls and others have relative urls.
             The relative urls need to be reformatted: 
@@ -29,11 +30,10 @@ class TaxSpider(scrapy.Spider):
             '''
             if next_page is None: continue                          # Ignore rows without links
 
-            if self.start_urls[0] not in next_page:                # Invalid relative url
+            if self.start_urls[0] not in next_page:                 # Invalid relative url
                 subpage = next_page.split('statutes/')[-1]          # Get last part of relative url
                 next_page = f'{self.start_urls[0]}{subpage}'        # Append absolute and relative url
             
-            logging.info(f'Next Page URL: {next_page}')
             yield response.follow(next_page, callback=self.parse)   # Recursively pass next page 
     
     # ===============================================================================
@@ -42,15 +42,22 @@ class TaxSpider(scrapy.Spider):
         print("===============================================================================")
         
         # Extract all the text withing the page section
-        data = res.xpath('//div[@class="section"]//text()').extract()
-        logging.info(f'\nResponse: {res}\nData: {data}')
+        #data = res.xpath('//div[@class="section"]//text()').extract()
 
-        # Collect target data here
-        yield None
-        '''yield {
-            'url': res,
-            'title': res.css('.title').get()
-        }'''
+        statute = res.xpath('//div[@class="section"]/h1/text()').get()          # Get statute title
+        section_number = res.xpath('//div[@class="section"]/@id').get()         # Get section number
+        subdiv_number = res.xpath('//div[@class="subd"]/@id').getall()          # Get subdivision number        
+        info = res.xpath('//div[@class="section"]/p/text()').getall()           # Main text description
+
+        logging.info(f'\nURL: {res}\nStatute: {statute}\nSection No: {section_number}\nSub-div No: {subdiv_number}')
+        tax_item = TaxItem()
+
+        tax_item['statute'] = statute
+        tax_item['section_number'] = section_number
+        tax_item['subdiv_number'] = subdiv_number
+        tax_item['info'] = info
+        
+        yield tax_item
 
 # Adjust the logging settings
 logging.getLogger('scrapy').propagate = False
